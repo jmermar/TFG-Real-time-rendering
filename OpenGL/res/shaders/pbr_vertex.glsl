b3b0819e163e7dd8808aa2653946e34caf1fed28
Jose Meroño <josesmerono@gmail.com>
2022-06-29 10:53:52 +0200

    Added untextured PBR

--- OpenGL/res/shaders/pbr_vertex.glsl
+++ OpenGL/res/shaders/pbr_vertex.glsl
@@ -1,128 +1,21 @@
 #version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 2) in vec3 aNormal;
+layout (location = 1) in vec2 aTexCoords;
 
-layout (location = 0) in vec3 vPos;
-layout (location = 1) in vec2 vTex;
-layout (location = 2) in vec3 vNorm;
+out vec2 TexCoords;
+out vec3 WorldPos;
+out vec3 Normal;
 
-out vec3 fColor;
-
-uniform mat4 modelMatrix;
-uniform mat4 viewMatrix;
 uniform mat4 projMatrix;
-
-uniform vec3 albedo;
-uniform float metallic;
-uniform float roughness;
-uniform float ao;
-
-uniform vec3 lightPositions[4];
-uniform vec3 lightColors[4];
-
-uniform vec3 camPos;
-
-const float PI = 3.14159265359;
-
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-    float a = roughness*roughness;
-    float a2 = a*a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-
-    return nom / denom;
-}
-// ----------------------------------------------------------------------------
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-
-    float nom   = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-
-    return nom / denom;
-}
-// ----------------------------------------------------------------------------
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
-
-    return ggx1 * ggx2;
-}
-// ----------------------------------------------------------------------------
-vec3 fresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
 
 void main()
 {
-    vec3 Normal = mat3(modelMatrix) * vNorm;
-    vec3 WorldPos = vec3(modelMatrix * vec4(vPos, 1));
-    vec3 N = normalize(Normal);
-    vec3 V = normalize(camPos - WorldPos);
+    TexCoords = aTexCoords;
+    WorldPos = vec3(modelMatrix * vec4(aPos, 1.0));
+    Normal = mat3(modelMatrix) * aNormal;   
 
-    // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0 
-    // of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)    
-    vec3 F0 = vec3(0.04); 
-    F0 = mix(F0, albedo, metallic);
-
-    // reflectance equation
-    vec3 Lo = vec3(0.0);
-    for(int i = 0; i < 4; ++i) 
-    {
-        // calculate per-light radiance
-        vec3 L = normalize(lightPositions[i] - WorldPos);
-        vec3 H = normalize(V + L);
-        float distance = length(lightPositions[i] - WorldPos);
-        float attenuation = 1.0 / (distance * distance);
-        vec3 radiance = lightColors[i] * attenuation;
-
-        // Cook-Torrance BRDF
-        float NDF = DistributionGGX(N, H, roughness);   
-        float G   = GeometrySmith(N, V, L, roughness);      
-        vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
-           
-        vec3 numerator    = NDF * G * F; 
-        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001; // + 0.0001 to prevent divide by zero
-        vec3 specular = numerator / denominator;
-        
-        // kS is equal to Fresnel
-        vec3 kS = F;
-        // for energy conservation, the diffuse and specular light can't
-        // be above 1.0 (unless the surface emits light); to preserve this
-        // relationship the diffuse component (kD) should equal 1.0 - kS.
-        vec3 kD = vec3(1.0) - kS;
-        // multiply kD by the inverse metalness such that only non-metals 
-        // have diffuse lighting, or a linear blend if partly metal (pure metals
-        // have no diffuse light).
-        kD *= 1.0 - metallic;	  
-
-        // scale light by NdotL
-        float NdotL = max(dot(N, L), 0.0);        
-
-        // add to outgoing radiance Lo
-        Lo += (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
-    }   
-    
-    // ambient lighting (note that the next IBL tutorial will replace 
-    // this ambient lighting with environment lighting).
-    vec3 ambient = vec3(0.03) * albedo * ao;
-
-    vec3 color = ambient + Lo;
-
-    // HDR tonemapping
-    color = color / (color + vec3(1.0));
-    // gamma correct
-    color = pow(color, vec3(1.0/2.2)); 
-    fColor = pow(color, vec3(1.0/2.2)); 
-
-    gl_Position = projMatrix * viewMatrix * modelMatrix * vec4(vPos, 1.0);
+    gl_Position =  projMatrix * viewMatrix * vec4(WorldPos, 1.0);
 }
--- OpenGL/src/mainTeapot.cpp
+++ OpenGL/src/mainTeapot.cpp
@@ -21,20 +21,16 @@ int main(int argc, char** argv) {
 
     glm::mat4 proj = glm::perspectiveFov(90.f, (float)Width, (float)Height, 0.1f, 1000.f);
 
-    MaterialTextured* mat = new MaterialTextured("res/textures/rusted-steel");
-    Mesh* mesh = new Mesh("res/models/cube.obj");
+    Mesh* teapot = new Mesh("res/models/teapot.obj");
+    Material* mat = new Material(glm::vec3(1, 0, 0), 1, 0.15, 1);
     mat->setProj(proj);
 
     vector<LightSource> lights;
-    lights.push_back({glm::vec3(-1, -1, 2), glm::vec3(150, 150, 150)});
-    lights.push_back({glm::vec3(1, 1, 2), glm::vec3(150, 150, 150)});
+    lights.push_back({glm::vec3(0, 0, 5c), glm::vec3(100)});
 
     mat->setLightSources(lights);
 
     Transform modelTransform;
-    modelTransform.position = glm::vec3(0, 0, 0);
-    modelTransform.scale = glm::vec3(3);
-    modelTransform.rotation = glm::vec3(0);
 
 
     float camDis = 5.f;
@@ -72,20 +68,18 @@ int main(int argc, char** argv) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 
-        // Draw cubes
         mat->start();
-        modelTransform.position = glm::vec3(0, 0, 0);
+        modelTransform.position = glm::vec3(0, -1, 0);
         modelTransform.scale = glm::vec3(1);
         modelTransform.rotation = glm::vec3(0);
 
-        mat->renderModel(mesh, modelTransform);
-
+        mat->renderModel(teapot, modelTransform);
         window->present();
 
         deltaTime = (SDL_GetTicks() - ticks) / 1000.f;
     }
 
-    delete mesh;
+    delete teapot;
     delete mat;
 
     return 0;
